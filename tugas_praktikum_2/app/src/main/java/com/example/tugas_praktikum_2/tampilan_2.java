37a7ff1490042316a45b282fed4fa400afa5740b
kelfinleo <kelfin1208@gmail.com>
2021-06-28 17:42:42 +0700

    Tugas_praktikum_2

---
+++ tugas_praktikum_2/app/src/main/java/com/example/tugas_praktikum_2/tampilan_2.java
@@ -0,0 +1,14 @@
+package com.example.tugas_praktikum_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tampilan_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_2);
+    }
+}
